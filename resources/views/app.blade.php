0afe4d933cd2ffb2b727c509e1209759fb5562c5
José Manuel Vázquez Sosa <josemanuelvazquezsosa@gmail.com>
2026-06-12 21:01:57 -0600

    Configuracion inertiaJs y conexión con Laravel

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zona 02 EDIME - Estrategias de desarrollo e innovación de materiales educativos</title>
+    @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+<body class="font-sans antialiased">
+    @inertia
+</body>
+</html>
