4dc34bc062770d885809ce398e384649ea2b8223
José Manuel Vázquez Sosa <josemanuelvazquezsosa@gmail.com>
2026-06-12 23:12:19 -0600

    Configuración estilos globales

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,4 @@
-import '../css/app.css';
+// Plugins y librerias
 import { createApp, h, type DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -6,6 +6,13 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 //Prime Vue
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+
+//Fonts
+import '@fontsource-variable/montserrat/wght-italic.css';
+import '@fontsource-variable/open-sans/wdth-italic.css';
+
+// css
+import '../css/app.css';
 
 createInertiaApp({
     resolve:(name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
